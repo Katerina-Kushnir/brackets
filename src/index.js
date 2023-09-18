b25096f93d08b2d81feb0b51135698d4b0b7167d
Katerina-Kushnir <93322468+Katerina-Kushnir@users.noreply.github.com>
2023-09-18 18:01:27 +0300

    feat: add function check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketPairs = new Map(bracketsConfig);
+  const stack = [];
+
+  for (const char of str) {
+    if (bracketPairs.has(char)) {
+      if (stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(bracketPairs.get(char));
+      }
+    } else {
+      if (stack.length === 0 || stack.pop() !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
+
+
